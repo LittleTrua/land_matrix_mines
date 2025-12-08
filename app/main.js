1f038025037f3614de55615791874d33691d4376
LittleTrua <tanouk67@gmail.com>
2025-12-08 11:32:34 +0100

    cercle_propor_ndeals

--- app/main.js
+++ app/main.js
@@ -10,6 +10,8 @@ import GeoJSON from 'ol/format/GeoJSON.js';
 import VectorLayer from 'ol/layer/Vector';
 import {Circle, Fill, Stroke, Style} from 'ol/style.js';
 
+const  =
+
 const coucheOSM = new TileLayer({
   source: new OSM()
 });
@@ -29,23 +31,40 @@ const sourceCentroid = new VectorSource({
   url: 'http://localhost:8080/geoserver/land_matrix/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=land_matrix%3Adeals_by_country_centroid&maxFeatures=50&outputFormat=application%2Fjson',
 });
 
-const styleCentroid = new Style({
-  image: new Circle({
-    radius: 40,
-    fill: new Fill({ color: 'white' }),
-    stroke: new Stroke({ color: 'blue', width: 5 }),
-  }),
-});
+// const styleCentroid = new Style({
+//   image: new Circle({
+//     radius: 40,
+//     fill: new Fill({ color: 'rgba(245, 156, 39, 0.5)'}),
+//     stroke: new Stroke({ color: 'rgba(224, 139, 31, 0.8)', width: 2 }),
+//   }),
+// });
+
+// const layerCentroid = new VectorLayer({
+//   source: sourceCentroid,
+//   style: styleCentroid
+// });
+
+function getStyleCentroid(feature) {
+  const nDeals = feature.get('n_deals');
+  const rayon = Math.sqrt(nDeals) * 8;
+  const style = new Style({
+    image: new Circle({
+      radius: rayon,
+      fill: new Fill({ color: 'rgba(245, 156, 39, 0.5)' }),
+      stroke: new Stroke({ color: 'rgba(224, 139, 31, 0.5)', width: 1 }),
+    }),
+  });
+  return style;
+}
 
 const layerCentroid = new VectorLayer({
   source: sourceCentroid,
-  style: styleCentroid
+  style: getStyleCentroid,
 });
-
 
 const map = new Map({
   target: 'map',
-  layers: [coucheOSM, layerDeals, layerCentroid],
+  layers: [coucheOSM, layerCentroid],
   view: new View({
     center: [0, 0],
     zoom: 2
