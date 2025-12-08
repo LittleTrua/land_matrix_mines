5452d9a767bef655b41b24133ee60d83fdd4f987
LittleTrua <tanouk67@gmail.com>
2025-12-08 09:55:02 +0100

    ajouter_coucheWFS

--- app/main.js
+++ app/main.js
@@ -5,6 +5,9 @@ import ImageLayer from 'ol/layer/Image.js';
 import TileLayer from 'ol/layer/Tile.js';
 import ImageWMS from 'ol/source/ImageWMS.js';
 import OSM from 'ol/source/OSM.js';
+import VectorSource from 'ol/source/Vector';
+import GeoJSON from 'ol/format/GeoJSON.js';
+import VectorLayer from 'ol/layer/Vector';
 
 const coucheOSM = new TileLayer({
   source: new OSM()
@@ -20,9 +23,18 @@ const maCouche = new ImageLayer({
   source : coucheDeals
 });
 
+const dealsByCountryCentroid = new VectorSource({
+  format: new GeoJSON(),
+  url: 'http://localhost:8080/geoserver/land_matrix/ows?service=WFS'
+});
+
+const layerCentroid = new VectorLayer({
+  source: sourceCentroid,
+});
+
 const map = new Map({
   target: 'map',
-  layers: [coucheOSM, maCouche],
+  layers: [coucheOSM, maCouche, layerCentroid],
   view: new View({
     center: [0, 0],
     zoom: 2
